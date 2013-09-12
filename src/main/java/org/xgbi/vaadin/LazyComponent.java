5fbabd406484042d4cd7e92b886386d0d6d6cfc5
Vincent Demeester <vincent+git@demeester.fr>
2013-09-12 22:33:06 +0200

    Update Tabsheet documentation

--- src/main/java/org/xgbi/vaadin/LazyComponent.java
+++ src/main/java/org/xgbi/vaadin/LazyComponent.java
@@ -1,15 +1,24 @@
 package org.xgbi.vaadin;
 
 import com.vaadin.ui.Component;
+import com.vaadin.ui.TabSheet;
 
 /**
+ * {@link LazyComponent} is an interface defining {@link Component} that is
+ * actually a placeholder for a real {@link Component}.
+ * <p>
+ * {@link LazyComponent} are used for component that have multiple
+ * {@link Component} but display only one ({@link TabSheet} for example).
+ * </p>
  * 
  * @author Vincent Demeester
- *
+ * 
  */
 public interface LazyComponent extends Component {
 
 	/**
+	 * Get the real {@link Component}.
+	 * 
 	 * @return
 	 */
 	Component getInnerComponent();
--- src/main/java/org/xgbi/vaadin/tabsheet/TabFactory.java
+++ src/main/java/org/xgbi/vaadin/tabsheet/TabFactory.java
@@ -6,18 +6,22 @@ package org.xgbi.vaadin.tabsheet;
 import java.util.Collection;
 
 import com.vaadin.ui.Component;
+import com.vaadin.ui.TabSheet.Tab;
 
 /**
+ * Class to build {@link Tab} and return them to be used by a
+ * {@link LazyTabsheet}.
+ * 
  * @author Houbeb Ben Othmene
  * @author Vincent Demeester
- * 
  */
 public interface TabFactory {
 
 	/**
 	 * Get the list of tab
+	 * 
 	 * @return
 	 */
 	Collection<Component> getTabs();
-	
+
 }
